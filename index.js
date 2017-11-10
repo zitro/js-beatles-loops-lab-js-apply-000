daf56ff7479cdd0c6a0eee630d193340977b03fe
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-10 20:16:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 function theBeatlesPlay(musicians, instruments){
 let arr1 = [];
 
-if(i=0,i<musicians,i++){
+for(i=0,i<musicians.length,i++){
     arr1.push = `${musicians} plays ${instruments}`
 }
 
 }
 
-function johnLennonFacts(facts){
+/*function johnLennonFacts(facts){
 let i = 0;
 let full = '';
   while (facts[i]) {
     full += facts[i] + '!!!';
     i++;
 }
-}
+}*/
